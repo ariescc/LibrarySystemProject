68b2fa63e2916e758a65d017ab625f2dc76b0259
susulianhua <982341816@qq.com>
2017-09-17 16:03:43 +0800

    页面布局 bootstrap

--- LibraryProject/LibraryProject/Models/User.cs
+++ LibraryProject/LibraryProject/Models/User.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace LibraryProject.Models
 {
@@ -10,8 +11,27 @@ namespace LibraryProject.Models
     {
         [Key]
         public int ID { get; set; }
+
+        [Required]
+        [Remote("CheckUserName", "Users")]
         public string UserName { get; set; }
+
+        [Required]
+        [StringLength(16,MinimumLength = 8,ErrorMessage ="密码为8位-16位")]
+        [RegularExpression(@"[A-Za-z0-9]+",ErrorMessage ="密码为数字和字母的组合")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
-        public DateTime CreateTime { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage ="两次输入的密码不同！")]
+        [DataType(DataType.Password)]
+        public string PasswordComfirm { get; set; }
+
+        [Required]
+        [RegularExpression(@"[0-9]+",ErrorMessage ="手机号码格式错误")]
+        public string PhoneNum { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z0-9.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",ErrorMessage ="邮箱格式错误")]
+        public string Email { get; set; }
     }
 }
