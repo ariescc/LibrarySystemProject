4663e5e3b04d506d64af0d781dd0e1e387078565
ygjch <wwworzzzzzzzzzz@gmail.com>
2017-09-22 22:24:27 +0800

    userPartial

---
+++ LibraryProject/LibraryProject/Models/BookType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryProject.Models
+{
+    public class BookType
+    {
+        public int ID { get; set; }
+        public string BookName { get; set; }
+        public int BookAmount { get; set; }
+    }
+}
